99be542e2af6869c12d684752021d9f112818b86
Amadou-Marong <marongamadou6@gmail.com>
2025-02-04 10:23:25 +0000

    modified the urls for production and development

--- frontend/src/components/QuizCard.jsx
+++ frontend/src/components/QuizCard.jsx
@@ -5,7 +5,7 @@ const QuizCard = ({quiz}) => {
   return (
     <div className="bg-white border-gray-200 border dark:bg-gray-800 rounded-lg shadow-md p-6">
         <div>
-        <img src={quiz.thumbnail} alt="quiz-image" className="w-full h-48 object-cover rounded-lg mb-4" />
+        <img src={quiz?.thumbnail} alt="quiz-image" className="w-full h-48 object-cover rounded-lg mb-4" />
         </div>
         <p className="text-gray-700 dark:text-gray-400 bg-gray-300 rounded-lg p-2">{quiz.title}</p>
     </div>
